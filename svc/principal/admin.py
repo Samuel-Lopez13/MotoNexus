cf3c7047123528cd4458355724cca2d609b861d2
Samuel-Lopez13 <lopez180219@gmail.com>
2024-04-26 00:33:05 -0600

    modelo productos

--- svc/principal/admin.py
+++ svc/principal/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Proveedores, Marcas
+from .models import Proveedores, Marcas, Productos
 
 admin.site.register(Proveedores)
 admin.site.register(Marcas)
+admin.site.register(Productos)
--- svc/principal/models.py
+++ svc/principal/models.py
@@ -20,3 +20,14 @@ class Marcas(models.Model):
 
     class Meta:
         db_table = 'principal_marcas'
+
+class Productos(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    img = models.FileField(upload_to='image-svc')
+    stock = models.IntegerField()
+    noVenta = models.IntegerField()
+    status = models.CharField(max_length=20)
+    proveedor = models.ForeignKey(Proveedores, on_delete=models.SET_NULL, null=True)  # Tambien esta el Cascade
+    marca = models.ForeignKey(Marcas, on_delete=models.SET_NULL, null=True)  # Tambien esta el Cascade
